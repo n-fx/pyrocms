cc879c7900540c826262ea2c9c50375efb3d7851
n-fx <nfx.gilo@gmail.com>
2018-12-14 10:50:15 +0100

    my first pyro project commit

---
+++ addons/default/morph/morph-theme/src/MorphTheme.php
@@ -0,0 +1,8 @@
+<?php namespace Morph\MorphTheme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+class MorphTheme extends Theme
+{
+
+}
---
+++ addons/default/morph/morph-theme/tests/Feature/MorphThemeTest.php
@@ -0,0 +1,10 @@
+<?php namespace Morph\MorphTheme\Test\Feature;
+
+class MorphThemeTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
